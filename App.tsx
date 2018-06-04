e7e1110b5b48dcefdf4a05ae5e76c95b7c3c0be7
kobayang <naokiod@gmail.com>
2018-06-05 01:52:22 +0900

    Hello

--- App.tsx
+++ App.tsx
@@ -5,9 +5,7 @@ export default class App extends React.Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.ts to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Hello, My name is Barry!</Text>
       </View>
     );
   }
